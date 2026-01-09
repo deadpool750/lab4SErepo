52ec48dbc70dbc71f2cf6eb6c1690eba874fd267
dpl750 <278435@student.pwr.edu.pl>
2026-01-09 12:05:43 +0100

    Problem Set 7: Part 1

--- medtracker/urls.py
+++ medtracker/urls.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Software engineering lab",
+        default_version="v1",
+        description="API documentation for the lab",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+    authentication_classes=[],
+)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("medtrackerapp.urls")),
+    path("api/swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
